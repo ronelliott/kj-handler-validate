d1d589c62af7004fe7ac1fb65bef29380c505c73
Ron Elliott <ron@elliott.im>
2015-09-29 13:42:38 -0700

    Removed joi from dependencies.

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 'use strict';
 
+var prequire = require('parent-require');
+
 module.exports = function($$resolver) {
-    !$$resolver.has('$joi') && $$resolver.add('$joi', require('joi'));
+    !$$resolver.has('$joi') && $$resolver.add('$joi', prequire('joi'));
     $$resolver.add('$validate', require('./handler'));
 };
